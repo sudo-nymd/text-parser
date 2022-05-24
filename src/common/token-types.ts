6212d23f64be144ed5f33286417b135d7387be60
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-05-24 00:25:19 -0500

    TokenTypes added.

---
+++ src/common/token-types.ts
@@ -0,0 +1,47 @@
+
+export enum TokenTypes {
+    Character = 'character',
+    Phrase = 'phrase',
+    Whitespace = 'whitespace',
+    Word = 'word',
+    Plugin = 'plugin'
+}
+
+export enum TokenSubTypes {
+    DoubleQuoted = `double-quoted`,
+    SingleQuoted = `single-quoted`,
+    Bracketed = 'bracketed',
+    Braced = 'braced',
+}
+
+export type Token = {
+    type: TokenTypes;
+    value: string;
+    subType: string;
+}
+
+export type PhraseToken = {
+    type: TokenTypes.Phrase;
+}
+
+export type PluginToken = Token & {
+    type: TokenTypes.Plugin;
+}
+
+export type CharacterToken = Token & {
+    type: TokenTypes.Character;
+}
+
+export type WordToken = Token & {
+    type: TokenTypes.Word;
+}
+
+export type WhitespaceToken = Token & {
+    type: TokenTypes.Whitespace;
+}
+
+export type TokenSpec = {
+    type: TokenTypes;
+    regex: RegExp;
+}
+
