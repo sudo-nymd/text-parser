a167eeb0931180828703575fafdb5f139acd7aed
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-05-24 00:25:50 -0500

    Test logging added.

---
+++ src/common/config.ts
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const getValueAsBoolean = (key: string, defVal = false) => {
+    return (process.env[key] === '1' || process.env[key] === 'true') ? true : defVal
+}
+
+export const config = {
+    DEBUGGING: getValueAsBoolean('DEBUG')
+}
