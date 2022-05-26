0f799fc43a3efc52942c50b8fea8ae4187dd76fa
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-05-26 16:53:44 -0500

    Export tokenizer at top-level

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 
 import { Parser } from "./parser";
+import { Tokenizer } from './tokenizer'
 import * as plugins from './plugins';
 
-export { Parser, plugins }
+export { Parser, Tokenizer, plugins }
